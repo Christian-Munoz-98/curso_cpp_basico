0a118b9468f80bbd3a03fc7743c1abda2aaeed06
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-01-12 13:34:57 -0600

    Limite ingresado por el usuario

---
+++ u4l3-ciclo-for/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int limite;
+
+    cout << "Ingresa el limite: ";
+    cin >> limite;
+
+    for (int i = 1; i <= limite ; i++)
+    {
+        cout << "Hola No." << i << endl;
+    }
+    
+}
