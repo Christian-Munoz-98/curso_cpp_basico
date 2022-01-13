c47ae04ecadf3288e66c64ee764cde9904a0ffa1
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-01-13 11:00:58 -0600

    Funciones

---
+++ u5l1-Funciones/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b)
+{
+   return a+b;
+}
+
+int multiplicacion(int a, int b)
+{
+    return a*b;
+}
+
+int main()
+{
+    cout << suma(2,200) << endl;
+    cout << multiplicacion(200,200);
+}
